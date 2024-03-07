71a357e2be1f489f862fe82e497712b79d8b52da
ZhangMystery <zjrzj97@163.com>
2024-03-07 16:42:17 +0800

    batch_size_modify

--- configs/_base_/schedules/cyclic-40e.py
+++ configs/_base_/schedules/cyclic-40e.py
@@ -63,5 +63,5 @@ test_cfg = dict()
 # Default setting for scaling LR automatically
 #   - `enable` means enable scaling LR automatically
 #       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (6 samples per GPU).
-auto_scale_lr = dict(enable=False, base_batch_size=1)
+#   - `base_batch_size` = (8 GPUs) x (6 samples per GPU) --> batch_size.
+auto_scale_lr = dict(enable=False, base_batch_size=16)
